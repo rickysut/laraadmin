3d5b40f0e33c69aa2c934ff3d377a89c5b1c45d7
Ricky Sutanto <ricky@sali-evo.com>
2022-08-15 16:32:23 +0700

    setup

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Keytech'),
+    'name' => env('APP_NAME', 'admin'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://keytech.test'),
+    'url' => env('APP_URL', 'http://laraadmin.test'),
 
     'asset_url' => env('ASSET_URL', null),
 
